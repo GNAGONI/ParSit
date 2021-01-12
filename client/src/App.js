173e8f45ad831540e5ff2c8dde5df7a642bea990
Nikita  Goncharov <nikita.goncharov@NBOOK146.local>
2021-01-12 13:52:00 +0300

    Implement client login, signup, account and basic todo functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,21 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Login from "./pages/login";
+import Signup from "./pages/signup";
+import Home from "./pages/home";
 import "./App.css";
 
-function App() {
-  return <div />;
-}
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={Signup} />
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
